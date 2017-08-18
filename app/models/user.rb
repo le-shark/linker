153c2dd1a9947987f3f89f9a35ac6f8b8b6fba37
Andrew <le_shark42@hotmail.com>
2017-08-18 08:53:52 +0200

    done basic User modeling/start working on the design

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  before_save { self.email = self.email.downcase }
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  NAME_REGEX = /^[a-zA-Z0-9\-\_]*$/i
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
+                       length: { maximum: 50 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    length: { maximum: 255 }, format: { with: EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :about_me, length: { maximum: 255 }
+  has_secure_password
+end

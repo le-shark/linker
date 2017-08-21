4d2d490144b90e4e0c579f8a489345eda0d1d6b0
Andrew <le_shark42@hotmail.com>
2017-08-21 21:26:46 +0200

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Thanks for registering, u/#{@user.username}! Enjoy exploring Linker!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation, :about_me)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'users/show'
+  get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
 
-  resources :u, controller: 'users'
+  resources :u, controller: 'users', as: 'users'
 end

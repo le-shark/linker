49b9077a7e4bfc3b7f40b138e18b8b4ca5767b76
Andrew <le_shark42@hotmail.com>
2017-09-13 14:41:45 +0200

    fix the authenticate? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,7 +63,7 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
-  def authenticated?(remember_digest, remember_token)
+  def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password? remember_token
   end
